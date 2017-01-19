a0f9dcba94f7fcfb6500feab5a3ae91c8d258180
Matthew <matt.parker@au.tribalworldwide.com>
2017-01-18 17:47:58 -0800

    mvc paged list and google map

---
+++ Webmap/Controllers/HomeController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Webmap.Models;
+using Newtonsoft.Json;
+
+namespace Webmap.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            var obj = new MapModel() {
+                id = 1,
+                name = "matty",
+                lat = "10.16596",
+                lng = "36.401081"
+            };
+
+            var obj1 = new MapModel()
+            {
+                id = 1,
+                name = "matty",
+                lat = "10.16596",
+                lng = "36.401081"
+            };
+
+
+            var obj2 = new MapModel()
+            {
+                id = 1,
+                name = "matty",
+                lat = "10.16596",
+                lng = "36.401081"
+            };
+
+
+            var obj3 = new MapModel()
+            {
+                id = 1,
+                name = "matty",
+                lat = "10.16596",
+                lng = "36.401081"
+            };
+
+
+            var obj4 = new MapModel()
+            {
+                id = 1,
+                name = "matty",
+                lat = "10.16596",
+                lng = "36.401081"
+            };
+
+
+
+            var obj5 = new MapModel()
+            {
+                id = 1,
+                name = "matty",
+                lat = "10.16596",
+                lng = "36.401081"
+            };
+
+
+
+            var obj6 = new MapModel()
+            {
+                id = 1,
+                name = "matty",
+                lat = "10.16596",
+                lng = "36.401081"
+            };
+
+
+
+            var obj7 = new MapModel()
+            {
+                id = 1,
+                name = "matty",
+                lat = "10.16596",
+                lng = "36.401081"
+            };
+
+
+
+
+            var objList = new List<MapModel>();
+            objList.Add(obj);
+            objList.Add(obj1);
+            objList.Add(obj2);
+            objList.Add(obj3);
+            objList.Add(obj4);
+            objList.Add(obj5);
+            objList.Add(obj6);
+            objList.Add(obj7);
+
+            var str = getMapJson(obj);
+
+            var strFromList = getMapJson(objList);
+
+
+
+            return View(objList);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        public string getMapJson<T>(T obj)
+        {
+            var str = JsonConvert.SerializeObject(obj);
+
+
+            return str;
+        }
+
+
+    }
+
+
+}
---
+++ Webmap/Models/MapModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Webmap.Models
+{
+    public class MapModel
+    {
+
+        public int id { get; set; }
+        public string name { get; set; }
+        public string lat { get; set; }
+        public string lng { get; set; }
+
+    }
+}
